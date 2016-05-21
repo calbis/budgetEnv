1a13c4d03ab8931d67deb100b96340669974b388
Calvin Bishop <calbis@gmail.com>
2016-05-20 21:42:42 -0400

    cleaning up code

--- routes/envelopes.js
+++ routes/envelopes.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require( 'express' );
 var db = require( '../lib/database.js' );
 var router = express.Router();
@@ -7,39 +9,33 @@ function SendEnvelopeSumDataByAccountId( response, accountId ) {
 	var q = "Select * From vw_envelope_sum_ext E Where E.AccountId = ?";
 	db.getRows( q, [accountId],
 		function( rows ) {
-			response.writeHead( 200, { 'content-type': 'application/json' } );
-			response.write( JSON.stringify( rows ) );
-			response.end( '\n' );
+			response.status( 200 ).send( rows );
 		},
 		function( status ) {
-			response.writeHead( status.code, { 'content-type': 'application/json' } );
-			response.write( JSON.stringify( status.message ) );
-			response.end( '\n' );
+			response.status( status.code ).send( status.message );
 		} );
 }
 
 
 router.post( '/',
-	function( req, res, next ) {
-		if ( req.user ) {
-			if ( ! isNaN( req.body.accountId ) && req.body.accountId >= 0 ) {
-				SendEnvelopeSumDataByAccountId( res, req.body.accountId );
-			} else if ( req.body.accountName.length > 0 ) {
-				var q = "Select Id From account Where Name = ?";
-				db.getRows( q, [req.body.accountName],
-					function( rows ) {
-						if ( rows.length > 0 ) {
-							SendEnvelopeSumDataByAccountId( res, rows[0].Id );
-						}
-					} );
-			} else {
-				res.writeHead( 400, { 'content-type': 'application/json' } );
-				res.write( JSON.stringify( "Missing valid account id or name" ) );
-				res.end( '\n' );
-			}
-		}
-		else {
+	function( req, res ) {
+		if ( ! req.user ) {
 			res.redirect( 401, '/' );
+			return;
+		}
+
+		if ( ! isNaN( req.body.accountId ) && req.body.accountId >= 0 ) {
+			SendEnvelopeSumDataByAccountId( res, req.body.accountId );
+		} else if ( req.body.accountName.length > 0 ) {
+			var q = "Select Id From account Where Name = ?";
+			db.getRows( q, [req.body.accountName],
+				function( rows ) {
+					if ( rows.length > 0 ) {
+						SendEnvelopeSumDataByAccountId( res, rows[0].Id );
+					}
+				} );
+		} else {
+			res.status( 400 ).send( "Missing valid account id or name" );
 		}
 	} );
 
