9070dd197b0e40da356ecaa146f2f40b84de5db3
Calvin Bishop <calbis@gmail.com>
2016-05-21 10:29:53 -0400

    Whitespace

--- tests/app.js
+++ tests/app.js
@@ -7,15 +7,16 @@ var expect = require( "chai" ).expect;
 var needle = require( "needle" );
 
 
-describe( "Budget Env Back End", function () {
-	describe( "Should get a response back from the server", function () {
-		it( "servers response", function ( done ) {
-			needle.get( helper.baseUrl, function ( err, res ) {
-				expect( err ).to.not.exist;
-				expect( res ).to.exist;
+describe( "Budget Env Back End", function() {
+	describe( "Should get a response back from the server", function() {
+		it( "servers response", function( done ) {
+			needle.get( helper.baseUrl,
+				function( err, res ) {
+					expect( err ).to.not.exist;
+					expect( res ).to.exist;
 
-				done();
-			} );
+					done();
+				} );
 		} );
 	} );
 
