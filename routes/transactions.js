3f4c7e3470961f6f92818fdb228484d7b44849ad
Calvin Bishop <calbis@gmail.com>
2016-05-21 18:25:42 -0400

    flipping logic when checking for an authorized user

--- routes/transactions.js
+++ routes/transactions.js
@@ -19,23 +19,23 @@ function SendTransactionsByAccountId( response, accountId ) {
 
 router.post( '/',
 	function( req, res ) {
-		if ( req.user ) {
-			if ( ! isNaN( req.body.accountId ) && req.body.accountId >= 0 ) {
-				SendTransactionsByAccountId( res, req.body.accountId );
-			} else if ( req.body.accountName.length > 0 ) {
-				var q = "Select Id From account Where Name = ?";
-				db.getRows( q, [req.body.accountName],
-					function( rows ) {
-						if ( rows.length > 0 ) {
-							SendTransactionsByAccountId( res, rows[0].Id );
-						}
-					} );
-			} else {
-				res.status( 400 ).send( "Missing valid account id or name" );
-			}
-		}
-		else {
+		if ( ! req.user ) {
 			res.redirect( 401, '/' );
+			return;
+		}
+
+		if ( ! isNaN( req.body.accountId ) && req.body.accountId >= 0 ) {
+			SendTransactionsByAccountId( res, req.body.accountId );
+		} else if ( req.body.accountName.length > 0 ) {
+			var q = "Select Id From account Where Name = ?";
+			db.getRows( q, [req.body.accountName],
+				function( rows ) {
+					if ( rows.length > 0 ) {
+						SendTransactionsByAccountId( res, rows[0].Id );
+					}
+				} );
+		} else {
+			res.status( 400 ).send( "Missing valid account id or name" );
 		}
 	} );
 
